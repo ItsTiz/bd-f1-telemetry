069664329982568d0bf67a8ca4772dc995858933
Tiziano Vuksan <tiziano.vuksan@studio.unibo.it>
2026-03-20 15:08:16 +0100

    feat: single telemetry file parsing exporting to csv

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -1,14 +1,17 @@
-import TestSimpleRDD.inputFile
+import TestSimpleRDD.{inputFile, outputDir}
 import org.apache.spark.{SparkConf, SparkContext}
 import parsing.DatasetParser
 import utils.Commons
 
 object Test {
     val inputFile = "/dataset/Miami_Grand_Prix/Sprint_Qualifying/VER/1_tel.json"
+    val outputDir = "/output/csv"
 
     def main(args: Array[String]): Unit = {
         implicit val sparkSession = Commons.initializeSparkSession("test_telemetry");
         val parser = new DatasetParser();
-        parser.parseFile(Commons.getDatasetPath("local", inputFile));
+        val dataFrame = parser.parseTelemetryFile(Commons.getDatasetPath("local", inputFile));
+        parser.toCSV(dataFrame, Commons.getDatasetPath("local", outputDir))
+
     }
 }
--- src/main/scala/parsing/DatasetParser.scala
+++ src/main/scala/parsing/DatasetParser.scala
@@ -1,16 +1,51 @@
 package parsing
 
 import org.apache.spark
-import org.apache.spark.sql.{SparkSession, DataFrame, SQLImplicits}
+import org.apache.spark.sql.{Column, DataFrame, SQLImplicits, SparkSession}
+import org.apache.spark.sql.functions
 
 class DatasetParser(implicit spark: SparkSession) {
-    def parseFile(path: String): DataFrame = {
+
+    implicit class DataFramePrinter(df: DataFrame) {
+        def prettyPrint(header: String): Unit = {
+            println(header)
+            println("Schema: ")
+            df.printSchema()
+            println("Show: ")
+            df.show()
+            println("---")
+        }
+    }
+
+    def parseTelemetryFile(path: String): DataFrame = {
         val dfReader = spark.read;
+        // sets reader to treat normal JSON files
         dfReader.option("multiLine", value = true);
         val resultDF = dfReader.json(path);
-        println("schema of " + path +": ")
-        resultDF.printSchema()
-        println("---")
+        resultDF.prettyPrint("schema of " + path +": ")
         resultDF;
+    }
+
+    def toCSV(dataFrame: DataFrame, path: String): Unit = {
+        import spark.implicits._
+        val dataFrameFlat = dataFrame.select("tel.*")
+        dataFrameFlat.prettyPrint("DataFrameFlat: ")
+
+        val columnsNames: Array[String] = dataFrameFlat
+            .columns
+            .filter(e => e != "dataKey")
+        val columns: Array[Column] = columnsNames.map(e => dataFrameFlat(e))
+
+        val zipped = functions.arrays_zip(columns:_*)
+        val dataFrameZipped = dataFrameFlat.withColumn("zipped", zipped);
+        dataFrameZipped.prettyPrint("dataFrameZipped: ")
+
+        val exploded = functions.inline(dataFrameZipped("zipped"))
+        //val columnsMap: Map[String, Column] = dataFrameFlat.columns.zip(exploded).toMap
+
+        val dataFrameExploded = dataFrameZipped.select(exploded.as(columnsNames.toSeq));
+        dataFrameExploded.prettyPrint("dataFrameExploded: ")
+
+        dataFrameExploded.write.csv(path)
     }
 }
